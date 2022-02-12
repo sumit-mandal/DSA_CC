299820a9071b9129fce2ce6b47b5c7cee1fa5f06
Sumit Mandal <63957082+sumit-mandal@users.noreply.github.com>
2022-02-12 11:53:46 +0530

    first commit

---
+++ .virtual_documents/Sum_Of_Array_element.ipynb.py
@@ -0,0 +1,12 @@
+def sum_array(arr,n):
+    return (sum(arr))
+
+
+
+arr = [2,3,4,15]
+n = len(arr)
+
+print(sum_array(arr,n))
+
+
+
---
+++ .virtual_documents/sum_of_digits.ipynb.py
@@ -0,0 +1,14 @@
+def getsum(n):
+    
+    sum = 0
+    while (n get_ipython().getoutput("= 0):")
+        sum = sum + int(n % 10)
+        n = int(n/10)
+        
+    return sum
+
+n = 687
+print(getsum(n)) 
+
+
+
